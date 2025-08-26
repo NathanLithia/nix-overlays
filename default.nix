ece9f8b91600d3c4cbb7672c8928a8bf5affa644
NathanLithia <10991641+NathanLithia@users.noreply.github.com>
2025-08-26 13:15:08 +0930

    add script references

--- default.nix
+++ default.nix
@@ -1,5 +1,11 @@
 self: super: {
   nathanlithia = {
     gallery-dl = super.callPackage ./packages/gallery-dl.nix {};
+
+    vdl = super.writeShellScriptBin "vdl" (builtins.readFile ./scripts/vdl.sh);
+    mvdl = super.writeShellScriptBin "mvdl" (builtins.readFile ./scripts/mvdl.sh);
+    mdl = super.writeShellScriptBin "mdl" (builtins.readFile ./scripts/mdl.sh);
+    gdl = super.writeShellScriptBin "gdl" (builtins.readFile ./scripts/gdl.sh);
+
   };
 }
