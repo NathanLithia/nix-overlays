dde4ff3c240a9ee6c876b20a19924c39ca4f2f00
NathanLithia <10991641+NathanLithia@users.noreply.github.com>
2025-09-27 10:46:04 +0930

    Add yt-dlp reference

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 self: super: {
   nathanlithia = {
     gallery-dl = super.callPackage ./packages/gallery-dl.nix {};
+    yt-dlp = super.callPackage ./packages/yt-dlp.nix {};
 
     vdl = super.writeShellScriptBin "vdl" (builtins.readFile ./scripts/vdl.sh);
     mvdl = super.writeShellScriptBin "mvdl" (builtins.readFile ./scripts/mvdl.sh);
