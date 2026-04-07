a5565fe8ce868fc9671feffdb1b974175b54d38f
NathanLithia <10991641+NathanLithia@users.noreply.github.com>
2026-04-07 16:52:23 +0930

    Custom ALVR

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@ self: super: {
   nathanlithia = {
     gallery-dl = super.callPackage ./packages/gallery-dl/package.nix {};
     yt-dlp = super.callPackage ./packages/yt-dlp/package.nix {};
+    alvr = super.callPackage ./packages/alvr/package.nix {};
 
     vdl = super.writeShellScriptBin "vdl" (builtins.readFile ./scripts/vdl.sh);
     mvdl = super.writeShellScriptBin "mvdl" (builtins.readFile ./scripts/mvdl.sh);
