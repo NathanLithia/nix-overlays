75e291a90d97e40be20bffe262370dd95717bea3
NathanLithia <10991641+NathanLithia@users.noreply.github.com>
2026-04-07 16:52:09 +0930

    new format

--- packages/gallery-dl/package.nix
+++ packages/gallery-dl/package.nix
@@ -1,62 +1,47 @@
 {
   lib,
-  fetchFromGitHub,
+  fetchurl,
   nix-update-script,
   yt-dlp,
   python3Packages,
 }:
-
-let
-  pname = "gallery-dl";
-  version = "1.31.0";
-in
 python3Packages.buildPythonApplication {
-  inherit pname version;
+  pname = "gallery-dl";
+  version = "1.31.10";
   pyproject = true;
-
-  src = fetchFromGitHub {
-    owner = "mikf";
-    repo = "gallery-dl";
-    tag = "v${version}";
-    hash = "sha256-XxsMm6IhtZdmp//2jYXna24UKn13opf1pOla0C5la2E=";
+  src = fetchurl {
+    url = "https://github.com/mikf/gallery-dl/releases/download/v1.31.10/gallery_dl-1.31.10.tar.gz";
+    hash = "sha256-04fEFCnhTCZG7rEChb9Hd88O09po7j8u3LpQXz4Te7c=";
   };
-
   build-system = [ python3Packages.setuptools ];
-
   dependencies = [
     python3Packages.requests
     python3Packages.pysocks
     yt-dlp
   ];
-
   nativeCheckInputs = [ python3Packages.pytestCheckHook ];
-
   disabledTests = [
     # requires network access
     "test_init"
   ];
-
-  pytestFlagsArray = [
+  disabledTestPaths = [
     # requires network access
-    "--ignore=test/test_results.py"
-    "--ignore=test/test_downloader.py"
-
+    "test/test_results.py"
+    "test/test_downloader.py"
     # incompatible with pytestCheckHook
-    "--ignore=test/test_ytdl.py"
+    "test/test_ytdl.py"
   ];
-
   pythonImportsCheck = [ "gallery_dl" ];
-
   passthru.updateScript = nix-update-script { };
-
   meta = {
-    changelog = "https://github.com/mikf/gallery-dl/blob/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/mikf/gallery-dl/blob/v1.31.10/CHANGELOG.md";
     description = "Command-line program to download image-galleries and -collections from several image hosting sites";
     homepage = "https://github.com/mikf/gallery-dl";
     license = lib.licenses.gpl2Only;
     mainProgram = "gallery-dl";
     maintainers = with lib.maintainers; [
       dawidsowa
+      FlameFlag
       lucasew
     ];
   };
